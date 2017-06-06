60bec48f80965cbb0836c673c99fb92acd0a26a2
justien <justinelera@gmail.com>
2017-06-06 20:43:43 +0200

    Saving

---
+++ t310/go/main/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+/*
+func xor(input1 []int, input2 []int) []int {
+ 
+var output_slice []int
+
+let n be the length of input1
+for i from 0 to n {
+    a is the nth element of input1
+    b is the nth element of input2
+    c is a XOR b
+    output_slice = append(output_slice, c)
+  }  
+    return output_slice
+}
+*/
+
+
+
+
+
+
+
+
+func factorial(x int) int {
+
+	fmt.Println("Factorial is called into being")
+	fmt.Println("x has value:", x)
+	
+	if x == 0 {
+	
+		return 1
+		
+	}
+	return x * factorial(x-1)
+}
+
+
+func main() {
+
+	fmt.Println(factorial(4))
+	
+}
